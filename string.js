b033b0a12949b5c3979eb66db25262fafc62d2f1
rimasumdev <67681547+rimasumdev@users.noreply.github.com>
2023-04-05 13:08:29 +0600

    js

--- string.js
+++ string.js
@@ -1,2 +1,9 @@
-let str = 'This is String Mathod Test';
-console.log(str.length);
+let str = 'Test Masum';
+console.log(str.padStart(21, ' '));
+// let count = 0;
+// for (let v of str) {
+//   if (v == 's') {
+//     count++;
+//   }
+// }
+// console.log(count);

ccddd7b6abcdee2be0c85e8e684956561f56403b
rimasumdev <67681547+rimasumdev@users.noreply.github.com>
2023-04-09 15:34:27 +0600

    js

--- string.js
+++ string.js
@@ -1,5 +1,19 @@
-let str = 'Test Masum';
-console.log(str.padStart(21, ' '));
+let str = 'This is as Modern Text';
+// console.log(str.padStart(21, ' '));
+let arr = str.split(' ');
+let isTrue = true;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] == 'as') {
+    // console.log('Data Found!');
+    isTrue = false;
+    break;
+  }
+}
+if (!isTrue) {
+  console.log('Data Found!');
+} else {
+  console.log('Data Not Found!');
+}
 // let count = 0;
 // for (let v of str) {
 //   if (v == 's') {

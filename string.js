9fb6ae72fe97771a5838da6290d73c31b57786a8
rimasumdev <67681547+rimasumdev@users.noreply.github.com>
2023-04-10 15:07:47 +0600

    js

--- string.js
+++ string.js
@@ -1,19 +1,25 @@
-let str = 'This is as Modern Text';
+const prompt = require('prompt-sync')({ sigint: true });
+let str = 'This is as ModernS Texts';
 // console.log(str.padStart(21, ' '));
-let arr = str.split(' ');
-let isTrue = true;
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] == 'as') {
-    // console.log('Data Found!');
-    isTrue = false;
-    break;
-  }
-}
-if (!isTrue) {
-  console.log('Data Found!');
-} else {
-  console.log('Data Not Found!');
-}
+// let arr = str.split(' ');
+// let isTrue = true;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] == 'as') {
+//     isTrue = false;
+//     break;
+//   }
+// }
+// if (!isTrue) {
+//   console.log('Data Found!');
+// } else {
+//   console.log('Data Not Found!');
+// }
+let input = prompt('Search Text : ');
+const reg = new RegExp(input, 'gi');
+let strCount = str.match(reg);
+console.log(
+  '"' + input + '"' + ' Word Found in ' + strCount.length + ' Places'
+);
 // let count = 0;
 // for (let v of str) {
 //   if (v == 's') {
--- test.js
+++ test.js
@@ -1,11 +1,18 @@
-const names = ['Masum', 'Raid', 'Ashik', 'Rima'];
-let index = -1;
-let name = names[++index];
-// console.log(name);
-setInterval(() => {
-  name = names[index++];
-  console.log(name, name.length);
-  if (index === name.length) {
-    index = 0;
-  }
-}, 1000);
+// const names = ['Masum', 'Raid', 'Ashik', 'Rima'];
+// let index = -1;
+// let name = names[++index];
+// // console.log(name);
+// setInterval(() => {
+//   name = names[index++];
+//   console.log(name, name.length);
+//   if (index === name.length) {
+//     index = 0;
+//   }
+// }, 1000);
+const prompt = require('prompt-sync')();
+let lang = prompt('What is your favorite programming language?');
+
+let feedback =
+  lang.toLowerCase() === 'javascript' ? `It's great!` : `It's ${lang}`;
+
+alert(feedback);

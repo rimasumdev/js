a3d640d944cbc20af3351c341dba6da3d60c6167
rimasumdev <67681547+rimasumdev@users.noreply.github.com>
2023-05-14 14:29:10 +0600

    jsedit

---
+++ indes.js
@@ -0,0 +1,13 @@
+function MasAlaMachine(who) {
+  if (who == 'Pir') {
+    alert("Ok, No Problems. It's Pir! You Know.");
+    document.write("Ok, No Problems. It's Pir! You Know.", '</br>');
+    console.log("Ok, No Problems. It's Pir! You Know.");
+  } else {
+    alert("No...., It's Harrrrammmm....");
+    document.write("No...., It's Harrrrammmm....");
+    console.log("No...., It's Harrrrammmm....");
+  }
+}
+MasAlaMachine('Pir');
+MasAlaMachine('Others');
